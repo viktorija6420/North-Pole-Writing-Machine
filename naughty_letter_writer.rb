6e969faa59264ae4e04e44b2e731f4dd097409e0
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-24 15:02:05 -0400

    done with naughty kids

--- naughty_letter_writer.rb
+++ naughty_letter_writer.rb
@@ -1,21 +1,22 @@
 require 'erb'
 
 kids_data   = File.read('data/kids-data.txt')
-nice_letter = File.read('templates/nice_letter_template.txt.erb')
+naughty_letter = File.read('templates/naughty_letter_template.txt.erb')
 
 kids_data.each_line do |kid|
-
+  
   kid_data_array = kid.split
 
   name     = kid_data_array[0]
   gender   = kid_data_array[1]
   behavior = kid_data_array[2]
   toys     = kid_data_array[3..8]
+  infraction = kid.split('|')[1]
 
-  next unless behavior == 'nice'
+  next unless behavior == 'naughty'
 
-  filename    = 'letters/nice/' + name + '.txt'
-  letter_text = ERB.new(nice_letter, nil,'-').result(binding)
+  filename    = 'letters/naughty/' + name + '.txt'
+  letter_text = ERB.new(naughty_letter, nil,'-').result(binding)
 
   puts "Writing #{filename}."
   File.write(filename, letter_text)
